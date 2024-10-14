98debb70c94ad1738735cc33298d460ffb772fec
José Sánchez-Gallego <gallegoj@uw.edu>
2024-10-13 17:03:23 -0700

    Updates

--- conf.d/venv_functions.fish
+++ conf.d/venv_functions.fish
@@ -39,9 +39,8 @@ function venv-activate -d "Activates a virtual environment from a centralised lo
     set -l venv_name $argv[1]
     set -q VENV_ROOT; and set -l venv_root $VENV_ROOT; or set -l venv_root $HOME/.local/share/virtualenv
 
-    source $venv_root/$venv_name/bin/activate.fish
-    # set -gx VIRTUAL_ENV $venv_root/$venv_name
-    # fish_add_path --path $venv_root/$venv_name/bin
+    set -gx VIRTUAL_ENV $venv_root/$venv_name
+    fish_add_path --path $venv_root/$venv_name/bin
 end
 
 function venv-reset -d "Resets the default virtual environment"
@@ -75,13 +74,9 @@ function venv-uv -d "Create a virtual environment with the given name and add mi
 
     echo "layout_uv" > .envrc
     direnv allow
-
-#     echo "[env]
-# _.python.venv = \"$venv_name\"
-# " > .mise.toml
-#     mise trust .mise.toml
 end
 
 function venv-list -d "List virtual environments in the centralised location"
-    find $VENV_ROOT -mindepth 1 -maxdepth 1 -type d -printf "%f\n"
+    set -q VENV_ROOT; and set -l venv_root $VENV_ROOT; or set -l venv_root $HOME/.local/share/virtualenv
+    find $venv_root -mindepth 1 -maxdepth 1 -type d -printf "%f\n"
 end
